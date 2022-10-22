9098a6b36d57c25babe07fac6c3f21ff1321d5fa
MarianPoirot <81marianp@gmail.com>
2022-10-22 16:13:32 +0200

    GameButton completed
    Note has not been changed nor verified

--- Scripts/World/GameButtonScript.gd
+++ Scripts/World/GameButtonScript.gd
@@ -4,8 +4,28 @@ var perfect=false;
 var great=false;
 var good=false;
 var ok=false;
+var note=null;	#note which will be given
+export var input =""
 
-
+func _unhandled_input(event):
+	if event.is_action_pressed(input):
+		if event.is_action_pressed(input, false):
+			if note!=null:
+				if perfect:
+					get_parent().increment_score(4)
+				elif great:
+					get_parent().increment_score(3)
+				elif good:
+					get_parent().increment_score(2)
+				elif ok:
+					get_parent().increment_score(1)
+			else:
+				get_parent().increment_score(0)
+		if event.is_action_pressed(input):
+			frame = 1	#frame of the animation pressed
+		elif event.is_action_released(input):
+			$PushTimer.start()	#enables the animation to goes on
+		
 func _on_PerfectArea_area_entered(area):
 	if area.is_in_group("note"):
 		perfect=true
@@ -29,11 +49,13 @@ func _on_GreatArea_area_exited(area):
 func _on_GoodArea_area_entered(area):
 	if area.is_in_group("note"):
 		good=true
+		note = area	#when you enter the largest area, you init note to give to non null
 
 
 func _on_GoodArea_area_exited(area):
 	if area.is_in_group("note"):
 		good=false
+		note = null	#when you exit the largest area, you put the note to give to null
 
 func _on_Timer_timeout():
-	pass
+	frame=0	#frame de l'animation
