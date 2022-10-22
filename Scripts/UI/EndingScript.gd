b2ff88ebb3dc498092aaeae57f0ed3178b3d697e
MarianPoirot <81marianp@gmail.com>
2022-10-22 13:06:14 +0200

    Ending

--- Scripts/UI/EndingScript.gd
+++ Scripts/UI/EndingScript.gd
@@ -1,16 +1,11 @@
-extends Node
+extends Control
 
+class_name UIEnding
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func hide():
+	for n in $CanvasLayer.get_children():
+		n.hide()
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func show():
+	for n in $CanvasLayer.get_children():
+		n.show()
