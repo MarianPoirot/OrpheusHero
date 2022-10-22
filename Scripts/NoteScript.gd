3641e715f65e4f8f77ed0ffc64dee2ecb77dd151
MarianPoirot <81marianp@gmail.com>
2022-10-22 18:16:36 +0200

    Changed colors and added CPUParticles

--- Scripts/NoteScript.gd
+++ Scripts/NoteScript.gd
@@ -7,7 +7,9 @@ const SPAWN_Y = -16
 const DIST_TO_TARGET = TARGET_Y - SPAWN_Y
 
 const LEFT_LANE_SPAWN = Vector2(120, SPAWN_Y)
+const MID_LEFT_LANE_SPAWN = Vector2(140, SPAWN_Y)
 const CENTRE_LANE_SPAWN = Vector2(160, SPAWN_Y)
+const MID_RIGHT_LANE_SPAWN = Vector2(180, SPAWN_Y)
 const RIGHT_LANE_SPAWN = Vector2(200, SPAWN_Y)
 
 var speed = 0
@@ -34,9 +36,15 @@ func initialize(lane):
 		position = LEFT_LANE_SPAWN
 	elif lane == 1:
 		$AnimatedSprite.frame = 1
-		position = CENTRE_LANE_SPAWN
+		position = MID_LEFT_LANE_SPAWN
 	elif lane == 2:
 		$AnimatedSprite.frame = 2
+		position = CENTRE_LANE_SPAWN
+	elif lane == 3:
+		$AnimatedSprite.frame = 3
+		position = MID_RIGHT_LANE_SPAWN
+	elif lane == 4:
+		$AnimatedSprite.frame = 4
 		position = RIGHT_LANE_SPAWN
 	else:
 		printerr("Invalid lane set for note: " + str(lane))
@@ -50,6 +58,9 @@ func destroy(score):
 	$AnimatedSprite.visible = false
 	$Timer.start()
 	hit = true
+	if score == 4:
+		$Node2D/Label.text = "PERFECT"
+		$Node2D/Label.modulate = Color("ffddbd")
 	if score == 3:
 		$Node2D/Label.text = "GREAT"
 		$Node2D/Label.modulate = Color("f6d6bd")
@@ -57,7 +68,7 @@ func destroy(score):
 		$Node2D/Label.text = "GOOD"
 		$Node2D/Label.modulate = Color("c3a38a")
 	elif score == 1:
-		$Node2D/Label.text = "OKAY"
+		$Node2D/Label.text = "OK"
 		$Node2D/Label.modulate = Color("997577")
 
 
