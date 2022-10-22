333447a75a0c6432f7436301dd598ddbbb230876
MarianPoirot <81marianp@gmail.com>
2022-10-22 12:07:43 +0200

    Font + intro lore

--- Scripts/UI/OpeningScript.gd
+++ Scripts/UI/OpeningScript.gd
@@ -1,16 +1,6 @@
-extends Node
+extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_TextureButton_button_down():
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear2db(1)) #replace 1 by $HSlider.value when you have one
+	if get_tree().change_scene("res://Scenes/UI/PlayingUI.tscn") != OK:
+		print ("Error passing from Opening to playing scenes")
