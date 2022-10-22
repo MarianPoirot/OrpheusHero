5c0e5f3f18e216e148a4ad709e4ca36a829f0be7
MarianPoirot <81marianp@gmail.com>
2022-10-22 15:21:34 +0200

    calculs beats + remplissage gamebutton

--- Scripts/World/GameButtonScript.gd
+++ Scripts/World/GameButtonScript.gd
@@ -1,30 +1,39 @@
 extends AnimatedSprite
 
+var perfect=false;
+var great=false;
+var good=false;
+var ok=false;
 
 
 func _on_PerfectArea_area_entered(area):
-	pass # Replace with function body.
+	if area.is_in_group("note"):
+		perfect=true
 
 
 func _on_PerfectArea_area_exited(area):
-	pass # Replace with function body.
+	if area.is_in_group("note"):
+		perfect=false
 
 
 func _on_GreatArea_area_entered(area):
-	pass # Replace with function body.
+	if area.is_in_group("note"):
+		great=true
 
 
 func _on_GreatArea_area_exited(area):
-	pass # Replace with function body.
+	if area.is_in_group("note"):
+		great=false
 
 
 func _on_GoodArea_area_entered(area):
-	pass # Replace with function body.
+	if area.is_in_group("note"):
+		good=true
 
 
 func _on_GoodArea_area_exited(area):
-	pass # Replace with function body.
-
+	if area.is_in_group("note"):
+		good=false
 
 func _on_Timer_timeout():
-	pass # Replace with function body.
+	pass
